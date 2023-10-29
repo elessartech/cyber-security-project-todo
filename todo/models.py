a4b6b1e1336508b5d957e1e408c7605ba613716c
Maksim Ilmast <maksim.ilmast@yandex.com>
2023-10-29 18:54:42 +0200

    initial commit

---
+++ todo/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    details = models.TextField()
+    date = models.DateTimeField(default=timezone.now)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
---
+++ todo/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from .forms import TodoForm, UserCreationForm, LoginForm
+from .models import Todo
+
+
+def index(request):
+    if request.method == "POST":
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+    item_list = []
+    if request.user.id:
+        item_list = Todo.objects.filter(creator=request.user).order_by("-date")
+    form = TodoForm()
+
+    page = {
+        "forms": form,
+        "list": item_list,
+        "title": "TODO LIST",
+    }
+    return render(request, "index.html", page)
+
+
+def remove(request, item_id):
+    item = Todo.objects.get(id=item_id)
+    item.delete()
+    messages.info(request, "Todo was deleted.")
+    return redirect("/")
+
+
+def user_signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+    else:
+        form = UserCreationForm()
+    return render(request, "signup.html", {"form": form})
+
+
+def user_login(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect("/")
+    else:
+        form = LoginForm()
+    return render(request, "login.html", {"form": form})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect("/")
